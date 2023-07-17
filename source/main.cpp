6330ee860ad494bf6df22195cfe03bcf7347dce5
YudongNiu <64293688+YudongNiu@users.noreply.github.com>
2023-07-17 14:17:53 +0800

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -30,20 +30,20 @@ int main(int argc, char* argv[]){
     else if(alg == "PerH+")  // finished
         Effective_prop_opt_personalized_cross(dataset, topr, std::stod(beta), "hp");
 
-    else if(alg == "mpcount")
-        MetaPathCount(dataset, 1);
-    else if(alg == "lensplit")
-        MetaPathLenSplit(dataset);
-    else if(alg == "hg_stats")
-        Effective_hg_stats(dataset);
-    else if(alg == "global_scale")
-        Scalability_prop_opt_global(dataset, topr, method, len);
-    else if(alg == "personalized_scale")
-        Scalability_prop_opt_personalized(dataset, topr, std::stod(beta), method, len);
-    else if(alg == "hg_scale")
-        Scalability_hg_greater_f1(dataset, std::stod(topr), method, len);
-    else if(alg == "matching_graph_time")
-        MatchingGraphTime(dataset);
-    else if(alg == "union")
-        Effective_hg_global_f1_by_union(dataset, std::stod(topr), method);
+    //else if(alg == "mpcount")
+    //    MetaPathCount(dataset, 1);
+    //else if(alg == "lensplit")
+    //    MetaPathLenSplit(dataset);
+    //else if(alg == "hg_stats")
+    //    Effective_hg_stats(dataset);
+    //else if(alg == "global_scale")
+    //    Scalability_prop_opt_global(dataset, topr, method, len);
+    //else if(alg == "personalized_scale")
+    //    Scalability_prop_opt_personalized(dataset, topr, std::stod(beta), method, len);
+    //else if(alg == "hg_scale")
+    //    Scalability_hg_greater_f1(dataset, std::stod(topr), method, len);
+    //else if(alg == "matching_graph_time")
+    //    MatchingGraphTime(dataset);
+    //else if(alg == "union")
+    //    Effective_hg_global_f1_by_union(dataset, std::stod(topr), method);
 }
